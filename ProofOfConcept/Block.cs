4498ec99044b497049bf68931ae0e8870796b2df
I. den Houting <iain1998@live.nl>
2019-09-24 22:05:32 +0200

    Added block class

---
+++ ProofOfConcept/Block.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ProofOfConcept
+{
+    public enum Colors { turquoise, blue, orange, yellow, green, purple, red};
+    class Block
+    {
+        public bool Filled = false; 
+        public readonly Colors Color;
+        public Block(Colors color)
+        {
+            Color = color;
+            Filled = true;
+        }
+
+        public Block()
+        {
+
+        }
+    }
+}
