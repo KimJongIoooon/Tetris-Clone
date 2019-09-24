e900504299bf34a944ecab68e09e1d2e7237c3f8
I. den Houting <iain1998@live.nl>
2019-09-24 22:07:15 +0200

    Implemented block array in game.cs.

--- ProofOfConcept/Game.cs
+++ ProofOfConcept/Game.cs
@@ -10,20 +10,32 @@ namespace ProofOfConcept
     class Game
     {
         List<int> Numbers = new List<int>();
-        public List<Point> Field = new List<Point>();
+        public Block[,] Field = new Block[9, 23];//x=9 y=23
         public int TickRate;//default 1 sec
         public Game()
         {
-            Field.Add(new Point(0, 0));
-            Field.Add(new Point(1, 0));
-            Field.Add(new Point(0, 1));
-            Field.Add(new Point(0, 3));
             TickRate = 1000; 
         }
 
         public void Tick()
         {
             
+        }
+
+        public void FillFieldTest() {
+            for (int x = 0; x < 9; x++)
+            {
+                for (int y = 0; y < 23; y++)
+                {
+                    Field[x, y] = new Block(Colors.red);
+                }
+
+            }
+            Field[0, 0] = new Block(Colors.purple);
+            Field[1, 9] = new Block(Colors.blue);
+            Field[2, 4] = new Block(Colors.yellow);
+            Field[3, 6] = new Block(Colors.green);
+
         }
     }
 }
